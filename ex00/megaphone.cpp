72ac1325714f550fb14ea8f244e8a3a1bbe49cc8
Yak <yassinekhayri123@gmail.com>
2023-12-17 14:20:35 +0100

    Done

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 16:55:01 by ykhayri           #+#    #+#             */
-/*   Updated: 2023/12/09 18:11:08 by ykhayri          ###   ########.fr       */
+/*   Updated: 2023/12/17 13:44:53 by ykhayri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 
 int main(int ac, char **av)
 {
+	std::string tmp;
+
 	if (ac == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
 		for (int i = 1; i < ac; i++)
 		{
-			for (unsigned int j = 0; j < std::strlen(av[i]); j++)
+			tmp = av[i];
+			for (unsigned int j = 0; j < tmp.length(); j++)
 			{
 				std::cout << (char) toupper(av[i][j]);
 			}
