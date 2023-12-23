bca7c72b4496869b1b676d799f1ed9dac1842d3c
Yak <yassinekhayri123@gmail.com>
2023-12-23 14:02:58 +0100

    Evrything is good now

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,13 +6,14 @@
 /*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 15:09:08 by ykhayri           #+#    #+#             */
-/*   Updated: 2023/12/23 12:32:59 by ykhayri          ###   ########.fr       */
+/*   Updated: 2023/12/23 14:02:02 by ykhayri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
 #include <algorithm>
 #include <iostream>
+#include <ctime>
 
 int	Account::_nbAccounts;
 int	Account::_totalAmount;
